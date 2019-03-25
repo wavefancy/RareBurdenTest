5c47af193d31b30fcf7fc7ecc60f82ac35934b29
Wallace Wang <wavefancy@gmail.com>
2019-03-25 07:43:09 -0400

    missing no update

--- Test/bash.sh
+++ Test/bash.sh
@@ -1,1 +1,1 @@
-java -jar ../target/RareBurden-1.0-SNAPSHOT-jar-with-dependencies.jar -g test.group.txt -v test.indels.missing.vcf.gz --test collapse -s samples.txt > out.test.txt
+java -jar ../target/RareBurden-1.0-SNAPSHOT-jar-with-dependencies.jar -g test.group.txt -v test.indels.missing.vcf.gz --test b.burden -s samples.txt > out.test.txt
