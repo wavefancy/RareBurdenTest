f2f3499f98440a673a6c69e56298d1f55d083aba
Wallace Wang <wavefancy@gmail.com>
2019-04-29 22:55:19 -0400

    fix the bug for compute rare scores.

--- Test/bash.sh
+++ Test/bash.sh
@@ -1,4 +1,6 @@
-#java -jar ../target/RareBurden-1.0-SNAPSHOT-jar-with-dependencies.jar -g test.group.txt -v test.indels.missing.vcf.gz --test b.burden -s samples.txt > out.test.txt
+wecho "
+    java -jar ../target/RareBurden-1.0-SNAPSHOT-jar-with-dependencies.jar -g test.group.txt -v test.indels.missing.vcf.gz --test b.burden -s samples.txt > out.test.txt
+"
 wecho "
     java -jar ../target/RareBurden-1.0-SNAPSHOT-jar-with-dependencies.jar
         -g ./j.group.LOFHC.gene.txt
