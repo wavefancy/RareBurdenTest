f67fa625aa0f898178c5b6746611dc01fe8cb821
Wallace Wang <wavefancy@gmail.com>
2019-06-10 15:33:27 -0400

    firth collapseFirth

--- EpactsPlugins/0.Download.sh
+++ EpactsPlugins/0.Download.sh
@@ -1,4 +1,9 @@
 # Firth corrected burden test from Jason.
 # individual score is the sum of genotype scores.
 # FILE: group.b.burdenFirth.R
+# /humgen/diabetes/t2d_exomes/lib/epacts/EPACTS-3.2.4/share/EPACTS
 wecho "scp $xfer:/medpop/esp2/wallace/tools/epacts/EPACTS_build/share/EPACTS/group.b.burdenFirth.R ./"
+
+# Firth corrected logistic regression for collapse method.
+# group.b.collapseFirth.R
+wecho "scp $xfer:/medpop/esp2/wallace/tools/epacts/EPACTS_build/share/EPACTS/group.b.collapseFirth.R ./"
